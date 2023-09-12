02cd3e9af5e6ab6943ce43a20090900ffa11b822
vicky-magiri <ndung'u.victor19@students.dkut.ac.ke>
2023-09-12 08:10:04 +0300

    Add github Url

--- src/main/java/com/hng/stage1/Domain/Constants.java
+++ src/main/java/com/hng/stage1/Domain/Constants.java
@@ -1,7 +1,7 @@
 package com.hng.stage1.Domain;
 
 public class Constants {
-    public static final String GITHUB_FILE_URL="";
-    public static final String GITHUB_REPO_URL="";
+    public static final String GITHUB_FILE_URL="https://github.com/v-magiri/HNG/blob/main/src/main/java/com/hng/stage1/Stage1Application.java";
+    public static final String GITHUB_REPO_URL="https://github.com/v-magiri/HNG";
 
 }
