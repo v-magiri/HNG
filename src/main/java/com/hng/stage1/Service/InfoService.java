30a4a6916282b075ca41dee4db72fc48d5d1e0f5
vicky-magiri <ndung'u.victor19@students.dkut.ac.ke>
2023-09-12 09:19:55 +0300

    Refactor Response

--- src/main/java/com/hng/stage1/Service/InfoService.java
+++ src/main/java/com/hng/stage1/Service/InfoService.java
@@ -4,19 +4,30 @@ import com.hng.stage1.Domain.Constants;
 import com.hng.stage1.Domain.Response;
 import org.springframework.stereotype.Service;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
 import java.time.temporal.ChronoField;
+import java.util.Locale;
 
 @Service
 public class InfoService {
+
     public Response getInfo(String track,String slackName){
         try{
+            DayOfWeek currentDay = DayOfWeek.of(LocalDateTime.now(ZoneOffset.UTC).get(ChronoField.DAY_OF_WEEK));
+            String currentDayTitleCase = currentDay.getDisplayName(TextStyle.FULL, Locale.US);
+
+            LocalDateTime utcNow = LocalDateTime.now(ZoneOffset.UTC);
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
+            String currentUtcTimeFormatted = utcNow.format(formatter);
             return new Response(
                     slackName,
-                    String.valueOf(LocalDate.now().getDayOfWeek()),
-                    String.valueOf(LocalDateTime.now(ZoneOffset.UTC)),
+                    currentDayTitleCase,
+                    currentUtcTimeFormatted,
                     track,
                     Constants.GITHUB_FILE_URL,
                     Constants.GITHUB_REPO_URL,
